e82bef94d12d96aeceaaf6f3b081d5f3c5cb27f3
Tim Newbold <timnewbold.research@gmail.com>
2024-06-25 13:36:27 +0100

    Updated code to prepare mapped estimates of explanatory variables - updated to use terra package, calculate natural habitat at different spatial grains, and add toxicity to pesticide maps

--- 1_PrepareMapData.R
+++ 1_PrepareMapData.R
@@ -33,7 +33,6 @@ outDir <- "1_PrepareMapData/"
 ## The downloaded data need to be in the following directory structure:
 ## 'PEST-CHEMGRIDS_v1_01_APR/GEOTIFF'
 
-
 # Create output directory, if it doesn't already exist
 if(!dir.exists(outDir)) dir.create(outDir)
 
@@ -46,125 +45,60 @@ t.start <- Sys.time()
 print(t.start)
 
 # Load required packages
-suppressMessages(suppressWarnings(library(rgdal)))
-suppressMessages(suppressWarnings(library(raster)))
+suppressMessages(suppressWarnings(library(terra)))
 
 # Print session information to log file
 sessionInfo()
 
 #### Natural Habitat Map ####
+cat('Natural habitat\n')
 
 # Define Behrmann equal-area projection
-behrCRS <- CRS('+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +datum=WGS84 +ellps=WGS84 +units=m +no_defs')
+behrCRS <- '+proj=cea +lon_0=0 +lat_ts=30 +x_0=0 +y_0=0 +datum=WGS84 +ellps=WGS84 +units=m +no_defs'
 
-cat('% natural habitat\n')
+# Read in mapped estimates of primary vegetation
+pri <- terra::rast(paste0(dataDir,"pri_1km_int"))
 
-# Read map of primary habitat
-pri <- readGDAL(paste(
-  dataDir,"pri_1km_int",sep=""),
-  silent = TRUE)
+# Read in mapped estimates of secondary vegetation
+sec <- terra::rast(paste0(dataDir,"sec_1km_int"))
 
-# Create natural-habitat map, initially containing coverage of primary habitats
-nat <- pri
+# Combine primary and secondary vegetation to create estimates of total natural habitat
+nat <- pri + sec
 
-# Clean-up
-rm(pri)
-gc()
+# Reproject to Behrmann equal-area projection with 1-km resolution
+nat <- terra::project(x = nat,y = behrCRS,res = 1000)
 
-# Read map of secondary habitat
-sec <- readGDAL(paste(
-  dataDir,"sec_1km_int",sep=""),
-  silent = TRUE)
+# Aggregate natural habitat estimates into 2- 5 and 10-km blocks
+nat.2 <- terra::aggregate(x = nat,fact = 2,fun = 'mean',na.rm = TRUE)
+nat.5 <- terra::aggregate(x = nat,fact = 5,fun = 'mean',na.rm = TRUE)
+nat.10 <- terra::aggregate(x = nat,fact = 10,fun = 'mean',na.rm = TRUE)
 
-# Sum primary and secondary habitat as natural habitat
-nat$band1 <- nat$band1 + sec$band1
-
-rm(sec)
-gc()
-
-# Convert to raster
-nat <- raster(nat)
-
-# Project natural habitat map to Behrmann equal-area projection
-nat <- raster::projectRaster(from = nat,res = 1000,crs = behrCRS)
-
-# Round back to integer values
-values(nat) <- round(values(nat))
-
-# Create raster to depict 5-km blocks
-toRas <- nat
-
-# Define longitudinal steps to create 5-km blocks
-step <- ncol(nat)/5
-
-# Define number of blocks in the latitudinal direction
-nBlocks <- ceiling(nrow(nat)/5)
-
-# Create raster values depicting the identity of 5-km blocks
-cat('Splitting map\n')
-
-for (block in 1:nBlocks){
-  
-  blockStart <- ((block-1) * step) + 1
-
-  inds <- rep(blockStart:(blockStart+step - 1),each=5)
-  
-  if (block != nBlocks){
-    inds <- rep(inds,5)
-  } else {
-    inds <- rep(inds,4)
-  }
-  
-  if(block==1){
-    blockVals <- inds
-  } else {
-    blockVals <- c(blockVals,inds)
-  }
-}
-
-# Assign block identity values to raster map
-values(toRas) <- blockVals
-
-# Summarize natural habitat values in 1-km map by 5-km blocks
-cat('Calculating values\n')
-
-zm <- zonal(nat,toRas)
-
-# Create a new 5-km-resolution raster to hold summarized natural-habitat values
-e <- extent(nat)
-e[3] <- e[3]-1000
-
-newRas <- raster(nrows=nBlocks,ncols=step,ext=e,crs=behrCRS)
-
-# Assign summarized natural-habitat values to new 5-km raster
-values(newRas) <- zm[,2]
-
-# Write final raster to file
-writeRaster(x = newRas,filename = paste(outDir,"PercentNatural.tif",sep=""),format="GTiff")
-
-# Clean-up
-rm(newRas,blockVals,nat,zm)
-gc()
+# Write out natural rasters showing percent natural habitat at all grid resolutions
+terra::writeRaster(x = nat,filename = paste0(outDir,"PercentNaturalHabitat1k.tif"))
+terra::writeRaster(x = nat.2,filename = paste0(outDir,"PercentNaturalHabitat2k.tif"))
+terra::writeRaster(x = nat.5,filename = paste0(outDir,"PercentNaturalHabitat5k.tif"))
+terra::writeRaster(x = nat.10,filename = paste0(outDir,"PercentNaturalHabitat10k.tif"))
 
 #### Fertilizer Map ####
+cat('Fertiliser\n')
 
 # List directories containing estimates of fertilizer application for 17 crops
 cropDirs <- dir(path = fertilizerDataDir,pattern = "Fertilizer_",full.names = TRUE)
 
 # Loop over directories for each crop, summing resulting fertilizer maps
-total.application <- sum(stack(lapply(cropDirs,function(cd){
+application.rate <- sum(rast(lapply(cropDirs,function(cd){
   
   cat(paste0(gsub("0_data/FertilizerCropSpecific_Geotiff/Fertilizer_","",cd),"\n"))
   
   # List files that contain N, P and K fertilizer application rates for the crop
-  dataFiles <- dir(path = cd,pattern = "_Total.tif",full.names = TRUE)
+  dataFiles <- dir(path = cd,pattern = "_Rate.tif",full.names = TRUE)
   dataFiles <- dataFiles[!(grepl("aux",dataFiles))]
   dataFiles <- dataFiles[!(grepl("ovr",dataFiles))]
   
   # Loop over the files for N, P and K fertilizer
-  fert.application <- sum(stack(lapply(dataFiles,function(f){
+  fert.application <- sum(rast(lapply(dataFiles,function(f){
     
-    appl <- raster(f)
+    appl <- rast(f)
     
     return(appl)
     
@@ -174,9 +108,13 @@ total.application <- sum(stack(lapply(cropDirs,function(cd){
   
 })),na.rm=TRUE)
 
-writeRaster(x = total.application,filename = paste0(outDir,"FertilizerMap.tif"),format="GTiff")
+writeRaster(x = application.rate,filename = paste0(outDir,"FertilizerMap.tif"))
 
 #### Pesticide Map ####
+cat("Pesticides\n")
+
+# Read table of LD50 values for honeybees
+haz_tab <- read.csv(paste0(dataDir, "/TABLE_LD50_PesticidePropertiesDatabase.csv"))
 
 # List all files in the directory of pesticide application maps for the year 2015
 pest.files <- list.files(pesticideDataDir, pattern = "2015")
@@ -186,32 +124,94 @@ pest.files_H <- pest.files[grep(pest.files, pattern = "2015_H")]
 pest.files_L <- pest.files[grep(pest.files, pattern = "2015_L")]
 
 # Stack the rasters for the high estimates
-pest_H <- stack(paste0(pesticideDataDir, "/", pest.files_H))
+pest_H <- rast(paste0(pesticideDataDir, "/", pest.files_H))
 
 # Reclassify the various no-data values as NA:
 # -2 = Water; -1.5 = No data; -1 = Banned or not approved substances
-pest_H <- reclassify(pest_H, matrix(
+pest_H <- classify(pest_H, matrix(
   data = c(-2, -1.5, -1, NA, NA, NA), byrow = F, ncol = 2), right = F)
 
-# Sum high estimates of pesticide application across all substances and all crops
-pest_H_total <- calc(x = pest_H, fun = sum, na.rm = TRUE)
+# For each substance applied to each crop, divide application rate by honeybee LD50s
+pest_tox_H <- rast(lapply(X = pest_H,FUN = function(pest.map){
+  
+  pest_name <- strsplit(x = names(pest.map),split = '_')[[1]][3]
+  
+  # extract the honeybee contact LD50 value from the table
+  LD50.contact <- haz_tab[haz_tab$pesticide == pest_name, "Honeybees_contact"]
+  LD50.oral <- haz_tab[haz_tab$pesticide == pest_name, "Honeybees_oral"]
+  
+  
+  if (!is.na(LD50.contact)){
+    # If LD50 contact estimate is available, use that preferentially
+    cat(paste0(pest_name," - CONTACT","\n"))
+    
+    tox.map <- pest.map/LD50.contact
+    
+  } else if (!is.na(LD50.oral)){
+    # Otherwise, if LD50 oral estimate is available, use that
+    cat(paste0(pest_name," - ORAL","\n"))
+    
+    tox.map <- pest.map/LD50.oral
+  } else {
+    # Otherwise, set this pesticide to zero
+    cat(paste0(pest_name," - MISSING","\n"))
+    
+    tox.map <- pest.map*0
+  }
+  
+  return(tox.map)
+}))
+
+# Sum estimates of pesticide toxicity across all substances and all crops
+pest_tox_H_total <- sum(pest_tox_H,na.rm = TRUE)
 
 # Output the final map for high estimates of total pesticide application rate
-writeRaster(pest_H_total, filename = paste0(outDir, "/Pesticide_totalAPR_High.tif"))
+writeRaster(pest_tox_H_total, filename = paste0(outDir, "/Pesticide_TotalToxicity_High.tif"))
 
 # Stack the rasters for the low estimates
-pest_L <- stack(paste0(pesticideDataDir, "/", pest.files_L))
+pest_L <- rast(paste0(pesticideDataDir, "/", pest.files_L))
 
 # Reclassify the various no-data values as NA:
 # -2 = Water; -1.5 = No data; -1 = Banned or not approved substances
-pest_L <- reclassify(pest_L, matrix(
+pest_L <- classify(pest_L, matrix(
   data = c(-2, -1.5, -1, NA, NA, NA), byrow = F, ncol = 2), right = F)
 
-# Sum low estimates of pesticide application across all substances and all crops
-pest_L_total <- calc(x = pest_L, fun = sum, na.rm = TRUE)
+# For each substance applied to each crop, divide application rate by honeybee LD50s
+pest_tox_L <- rast(lapply(X = pest_L,FUN = function(pest.map){
+  
+  pest_name <- strsplit(x = names(pest.map),split = '_')[[1]][3]
+  
+  # extract the honeybee contact LD50 value from the table
+  LD50.contact <- haz_tab[haz_tab$pesticide == pest_name, "Honeybees_contact"]
+  LD50.oral <- haz_tab[haz_tab$pesticide == pest_name, "Honeybees_oral"]
+  
+  
+  if (!is.na(LD50.contact)){
+    # If LD50 contact estimate is available, use that preferentially
+    cat(paste0(pest_name," - CONTACT","\n"))
+    
+    tox.map <- pest.map/LD50.contact
+    
+  } else if (!is.na(LD50.oral)){
+    # Otherwise, if LD50 oral estimate is available, use that
+    cat(paste0(pest_name," - ORAL","\n"))
+    
+    tox.map <- pest.map/LD50.oral
+  } else {
+    # Otherwise, set this pesticide to zero
+    cat(paste0(pest_name," - MISSING","\n"))
+    
+    tox.map <- pest.map*0
+  }
+  
+  return(tox.map)
+}))
+
+# Sum estimates of pesticide toxicity across all substances and all crops
+pest_tox_L_total <- sum(pest_tox_L,na.rm = TRUE)
 
 # Output the final map for low estimates of total pesticide application rate
-writeRaster(pest_L_total, filename = paste0(outDir, "/Pesticide_totalAPR_Low.tif"))
+writeRaster(pest_tox_L_total, filename = paste0(outDir, "/Pesticide_TotalToxicity_Low.tif"))
 
 #### Finish ####
 
